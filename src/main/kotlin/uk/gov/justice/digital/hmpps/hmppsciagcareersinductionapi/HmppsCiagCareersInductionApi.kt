bcc7d78a452b897f1c8fd1c1b7edcd14faa2436c
theDustRoom <theDustRoom@users.noreply.github.com>
2023-03-28 13:42:10 +0000

    updating project name after deployment from template repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/HmppsCiagCareersInductionApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/HmppsCiagCareersInductionApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.hmppsciagcareersinductionapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class HmppsCiagCareersInductionApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<HmppsCiagCareersInductionApi>(*args)
 }
