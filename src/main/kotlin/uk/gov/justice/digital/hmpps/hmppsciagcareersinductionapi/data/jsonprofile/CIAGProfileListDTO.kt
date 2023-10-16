32ba338644cbb06dfae08db3682e0eee2374ef71
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2023-10-16 11:05:17 +0100

    completed Rebase

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/data/jsonprofile/CIAGProfileListDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/data/jsonprofile/CIAGProfileListDTO.kt
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 data class CIAGProfileListDTO(
 
-  @Schema(description = "This is the ID of the inmate ", name = "offenderId", pattern = "^[A-Z]\\d{4}[A-Z]{2}\$", required = true)
+  @Schema(description = "This is the List of inductions for inmates ", name = "offenderId", pattern = "^[A-Z]\\d{4}[A-Z]{2}\$", required = true)
   val ciagProfileList: List<CIAGProfileDTO>?,
 )
