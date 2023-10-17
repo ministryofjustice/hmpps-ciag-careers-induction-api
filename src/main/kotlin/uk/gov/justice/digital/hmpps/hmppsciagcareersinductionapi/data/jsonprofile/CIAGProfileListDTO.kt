7e46896047c4c74fc66336496ecdc2fd7d79f721
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-10-17 11:24:13 +0100

    RR-395 - removwed pattern attribute as not relevant to data type

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/data/jsonprofile/CIAGProfileListDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/data/jsonprofile/CIAGProfileListDTO.kt
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 data class CIAGProfileListDTO(
 
-  @Schema(description = "This is the List of inductions for inmates ", name = "ciagProfileList", pattern = "^[A-Z]\\d{4}[A-Z]{2}\$", required = true)
+  @Schema(description = "This is the List of inductions for inmates ", name = "ciagProfileList", required = true)
   val ciagProfileList: List<CIAGProfileDTO>?,
 )
