b593f893d5f87d04d129630b075d2e81c1cf9642
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2023-11-17 05:34:23 +0000

    added telemetry service

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/telemetry/TelemetryEvent.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.hmppsciagcareersinductionapi.telemetry
+
+enum class TelemetryEvent {
+  INDUCTION_CREATED,
+  INDUCTION_UPDATED,
+  DESIRE_TO_WORK,
+  HOPE_TO_GET_WORK,
+  WORK_TYPE,
+  ABILITY_TO_WORK,
+}
