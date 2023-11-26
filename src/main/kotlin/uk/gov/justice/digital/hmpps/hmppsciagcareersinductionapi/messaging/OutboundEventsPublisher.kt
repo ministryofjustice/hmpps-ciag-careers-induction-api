e4182f557e244cebe6f02c0f287c63d944aea2cf
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2023-11-26 12:56:04 +0000

    migrated to the latest vesrions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/messaging/OutboundEventsPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/messaging/OutboundEventsPublisher.kt
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsciagcareersinductionapi.messaging
 
-import com.amazonaws.services.sns.model.MessageAttributeValue
-import com.amazonaws.services.sns.model.PublishRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue
+import software.amazon.awssdk.services.sns.model.PublishRequest
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import java.util.*
 import java.util.function.Supplier
@@ -29,15 +29,18 @@ class OutboundEventsPublisher(
 
   fun publishToTopic(outboundEvent: OutboundEvent) {
     try {
-      val outboundEventMessageAttribute =
-        MessageAttributeValue().withDataType("String").withStringValue(outboundEvent.eventType.eventType)
-
-      val publishRequest = PublishRequest(topicArn, objectMapper.writeValueAsString(outboundEvent))
-        .withMessageAttributes(
-          Collections.singletonMap(EVENT_TYPE_KEY, outboundEventMessageAttribute),
-        )
-
-      topicSnsClient.publish(publishRequest)
+      topicSnsClient.publish(
+        PublishRequest.builder()
+          .topicArn(hmppsDomainTopic.arn)
+          .message(objectMapper.writeValueAsString(outboundEvent))
+          .messageAttributes(
+            mapOf(
+              "eventType" to MessageAttributeValue.builder().dataType("String").stringValue(outboundEvent.eventType.eventType).build(),
+            ),
+          )
+          .build()
+          .also { log.info("Published event $outboundEvent to outbound topic") },
+      )
     } catch (e: Throwable) {
       val message = "Failed (publishToTopic) to publish Event $outboundEvent.eventType to $TOPIC_ID"
       log.error(message, e)
