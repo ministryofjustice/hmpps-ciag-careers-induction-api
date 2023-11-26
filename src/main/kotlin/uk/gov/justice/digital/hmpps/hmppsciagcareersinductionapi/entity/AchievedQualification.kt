0debbb2e6c1f68d60e5bd671bc5e81bb5f02cefb
Sacintha Raj <sacintha.raj@digital.justice.gov.uk>
2023-11-26 12:56:04 +0000

    migrated to the latest vesrions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/entity/AchievedQualification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsciagcareersinductionapi/entity/AchievedQualification.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsciagcareersinductionapi.entity
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.persistence.Embeddable
 import uk.gov.justice.digital.hmpps.hmppsciagcareersinductionapi.data.common.QualificationLevel
-import javax.persistence.Embeddable
 
 @Embeddable
 data class AchievedQualification(
@@ -13,6 +13,30 @@ data class AchievedQualification(
   @Schema(description = "This is the level of  the subject the inmate has chosen.", name = "level", required = false)
   var level: QualificationLevel?,
 ) {
+  /*override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as AchievedQualification
+
+    if (subject != other.subject) return false
+    if (grade != other.grade) return false
+    if (level != other.level) return false
+
+    return true
+  }*/
+
+  override fun hashCode(): Int {
+    var result = subject?.hashCode() ?: 0
+    result = 31 * result + (grade?.hashCode() ?: 0)
+    result = 31 * result + (level?.hashCode() ?: 0)
+    return result
+  }
+
+  override fun toString(): String {
+    return "AchievedQualification(subject=$subject, grade=$grade, level=$level)"
+  }
+
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
@@ -24,16 +48,5 @@ data class AchievedQualification(
     if (level != other.level) return false
 
     return true
-  }
-
-  override fun hashCode(): Int {
-    var result = subject?.hashCode() ?: 0
-    result = 31 * result + (grade?.hashCode() ?: 0)
-    result = 31 * result + (level?.hashCode() ?: 0)
-    return result
-  }
-
-  override fun toString(): String {
-    return "AchievedQualification(subject=$subject, grade=$grade, level=$level)"
   }
 }
